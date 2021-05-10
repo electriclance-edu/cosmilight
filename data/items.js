bb7c3004c7fc3ec502cc061efea89bd7b72a39f1
electriclance <lance_libatique@hotmail.com>
2021-05-10 19:28:45 +0800

    basic file commit, goods purchasing styling
    + some work on item data structure

---
+++ data/items.js
@@ -0,0 +1,113 @@
+/*
+  {
+    name:"default",
+    desc:"default",
+    tags:["default"], //special tags that affect gameplay, eg. RESTRICTED, HANDCRAFTED, FORAGED
+    categories:["default"], //medical, food, literature, etc.
+    storage:"default", //crate, drum, etc.
+    rarity:"default", //common, rare, etc.
+    price:12345, //per unit
+  },
+*/
+var items = [
+  {
+    name:"default",
+    desc:"default",
+    tags:["default"],
+    categories:["default"],
+    storage:"default",
+    rarity:"default",
+    price:12345,
+  },
+  {
+    name:"KALceride",
+    desc:"An instant energy drink perfect for the rapid Ielern lifestyle.",
+    tags:[".factorymade"],
+    categories:["food"],
+    storage:"drum",
+    rarity:"common",
+    price:150,
+  },
+  {
+    name:"Profuse",
+    desc:"A chemical mixture designed for *Floral* species consumption.",
+    tags:[".factorymade"],
+    categories:["food"],
+    storage:"drum",
+    rarity:"common",
+    price:150,
+  },
+  {
+    name:"Rejuvenation Stone",
+    desc:"Heals minor injuries once swallowed. Tastes like blood.",
+    tags:[".factorymade"],
+    categories:["medical"],
+    storage:"crate",
+    rarity:"common",
+    price:200,
+  },
+  {
+    name:"Kyne Batteries",
+    desc:"Supercompact energy storage that can power a desk lamp for a thousand years.",
+    tags:["restricted",".factorymade"],
+    categories:["utility"],
+    storage:"crate",
+    rarity:"common",
+    price:750,
+  },
+  {
+    name:"Generic Cookies",
+    desc:"The unnamed store-brand type.",
+    tags:[".factorymade"],
+    categories:["food"],
+    storage:"crate",
+    rarity:"common",
+    price:20,
+  },
+  {
+    name:"Mineral Water",
+    desc:"",
+    tags:["default"],
+    categories:["default"],
+    storage:"default",
+    rarity:"default",
+    price:12345,
+  },
+  {
+    name:"default",
+    desc:"default",
+    tags:["default"],
+    categories:["default"],
+    storage:"default",
+    rarity:"default",
+    price:12345,
+  },
+  {
+    name:"default",
+    desc:"default",
+    tags:["default"],
+    categories:["default"],
+    storage:"default",
+    rarity:"default",
+    price:12345,
+  },
+  {
+    name:"default",
+    desc:"default",
+    tags:["default"],
+    categories:["default"],
+    storage:"default",
+    rarity:"default",
+    price:12345,
+  },
+  {
+    name:"default",
+    desc:"default",
+    tags:["default"],
+    categories:["default"],
+    storage:"default",
+    rarity:"default",
+    price:12345,
+  },
+
+]
